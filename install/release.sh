3332bf2ec2354c26ae0c90dd5bb66bb2eaa717cc
Pawel 'felixd' Wojciechowski <felixd@konopnickiej.com>
2018-06-12 02:05:27 +0200

    .gitignore checking during installation

--- install/release.sh
+++ install/release.sh
@@ -11,11 +11,21 @@ else
  git clone https://gitlab.com/felixd/git-releaser.git
 
  if [ -f "release.sh" ]; then
+  echo "Giving execution attributes to: release.sh"
   chmod +x release.sh
  fi
 
+ echo "Checking if .gitignore ignores [git-releaser/] folder"
  if [ ! -f ".gitignore" ]; then
+   echo ".gitignore does not exist. Creating it + ignoring git-releaser/ folder."
    echo "git-releaser/" >> .gitignore
+ else
+   if grep -R "git-releaser/" .gitignore; then
+     echo "It already ignores it. No need to change anything."
+   else
+     echo "File exists but does not ignore [git-releaser/]. Changing that."
+     echo "git-releaser/" >> .gitignore
+   fi
  fi
 
  if [ ! -f "VERSION" ]; then
